afd9a33bfa2bf14d7cf1d51734a691918bb3389d
keithkw <keithkw@hk1.ibm.com>
2017-11-28 16:48:18 +0800

    Commit2

---
+++ test-api/test-api-framework-core/src/main/java/com/company/test/api/framework/core/Test.java
@@ -0,0 +1,15 @@
+package com.company.test.api.framework.core;
+
+public class Test {
+
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+}
